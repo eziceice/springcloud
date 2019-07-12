c6a96dd990c46a568571551f932555312c1b566a
eziceice <liyutian6@gmail.com>
2019-07-12 14:57:06 +0800

    Fix Annotation issue

--- mall/src/main/java/com/springboot/mall/MallApplication.java
+++ mall/src/main/java/com/springboot/mall/MallApplication.java
@@ -4,12 +4,14 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.client.RestTemplate;
 
 @SpringCloudApplication
+@EnableFeignClients
 @MapperScan(annotationClass = Repository.class, basePackages = "com.springboot.mall")
 @EnableScheduling
 public class MallApplication {
