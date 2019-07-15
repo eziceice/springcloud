77bd1fb8fb64e7496179b74a3b24fa97da9372ad
eziceice <liyutian6@gmail.com>
2019-07-15 13:56:40 +0800

    Add Ribbon and RestTemplate Example.

--- mall/src/main/java/com/springboot/mall/MallApplication.java
+++ mall/src/main/java/com/springboot/mall/MallApplication.java
@@ -3,12 +3,9 @@ package com.springboot.mall;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.client.RestTemplate;
 
 @SpringCloudApplication
 @EnableFeignClients
@@ -18,12 +15,5 @@ public class MallApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MallApplication.class, args);
-    }
-
-
-    @Bean
-    @LoadBalanced
-    public RestTemplate initRestTemplate() {
-        return new RestTemplate();
     }
 }
--- mall/src/main/java/com/springboot/mall/controller/ProductController.java
+++ mall/src/main/java/com/springboot/mall/controller/ProductController.java
@@ -5,6 +5,8 @@ import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import com.springboot.mall.pojo.User;
 import com.springboot.mall.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
@@ -14,6 +16,9 @@ public class ProductController {
 
     @Autowired
     private RestTemplate restTemplate;
+
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
 
     @Autowired
     private UserService userService;
@@ -63,5 +68,22 @@ public class ProductController {
 
     public String error() {
         return "Timeout!";
+    }
+
+    @GetMapping("/loadBalancer")
+    public String testLoadBalancerClient() {
+        ServiceInstance instance = loadBalancerClient.choose("USER");
+        return instance.getHost() + ":" + instance.getPort();
+    }
+
+    /**
+     * Ribbon manage the registry service lists itself, not from Eureka
+     *
+     * @return
+     */
+    @GetMapping("/testWithoutEureka")
+    public String testWithoutEureka() {
+        ServiceInstance instance = loadBalancerClient.choose("users");
+        return instance.getHost() + ":" + instance.getPort();
     }
 }
