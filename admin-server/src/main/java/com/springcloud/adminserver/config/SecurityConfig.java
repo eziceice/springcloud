5ac1d9e6dfb4009958dee054ed398f73e028a73b
eziceice <liyutian6@gmail.com>
2019-07-28 20:26:57 +1000

    Add Spring Cloud Ribbon Example

---
+++ admin-server/src/main/java/com/springcloud/adminserver/config/SecurityConfig.java
@@ -0,0 +1,18 @@
+package com.springcloud.adminserver.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf().disable();
+        httpSecurity.formLogin().loginPage("/login.html").defaultSuccessUrl("/", true).permitAll();
+        httpSecurity.logout().logoutUrl("/logout");
+        httpSecurity.authorizeRequests().antMatchers("/login.html", "/**/**.css", "img/**", "/thrid-party/**").permitAll();
+        httpSecurity.authorizeRequests().antMatchers("/**").authenticated();
+    }
+}
