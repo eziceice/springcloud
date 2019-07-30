544f5a31422f220d141732624030b4015ac1ca62
eziceice <liyutian6@gmail.com>
2019-07-30 23:26:05 +1000

    Add Spring Cloud OAuth2 Example.

--- admin-server/src/main/java/com/springcloud/adminserver/config/SecurityConfig.java
+++ admin-server/src/main/java/com/springcloud/adminserver/config/SecurityConfig.java
@@ -1,20 +1,24 @@
 package com.springcloud.adminserver.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
 @Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf().disable();
-        httpSecurity.formLogin().loginPage("/login.html").defaultSuccessUrl("/", true).permitAll();
-        httpSecurity.logout().logoutUrl("/logout");
+        httpSecurity.formLogin().loginPage("/login.html").defaultSuccessUrl("/", true).failureUrl("/login-error").permitAll();
+        httpSecurity.logout().logoutUrl("/logout").logoutSuccessUrl("/");
         httpSecurity.authorizeRequests().antMatchers("/login.html", "/**/**.css", "img/**", "/thrid-party/**").permitAll();
         httpSecurity.authorizeRequests().antMatchers("/**").authenticated();
+
+        httpSecurity.exceptionHandling().accessDeniedPage("/401");
     }
 }
